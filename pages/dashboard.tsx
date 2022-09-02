a9dfebe5d692532c7779e510e7e876c49d376746
David <emekachinye2020@gmail.com>
2022-09-02 09:17:45 +0100

    Added responsive styling to dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,6 @@
 import { Style } from '@mui/icons-material';
 import { Button, Typography } from '@mui/material';
-import { Head, Main } from 'next/document'
+import Head from 'next/head'
 import React from 'react'
 import { FaPlay, FaEye } from "react-icons/fa";
 import Header from '../components/Header'
@@ -9,10 +9,26 @@ import styles from '../styles/dashboard.module.css'
 
 
 function dashBoard() { 
+  // if (auth2.isSignedIn.get()) {
+  //   var profile = auth2.currentUser.get().getBasicProfile();
+  //   console.log("ID: " + profile.getId());
+  //   console.log("Full Name: " + profile.getName());
+  //   console.log("Given Name: " + profile.getGivenName());
+  //   console.log("Family Name: " + profile.getFamilyName());
+  //   console.log("Image URL: " + profile.getImageUrl());
+  //   console.log("Email: " + profile.getEmail());
+  // }
   return (
     <div>
+      <Head>
+        <title>Dashboard</title>
+        <meta name="description" content="the dashboard page" />
+        {/* <meta name="viewport" content="width=device-width"></meta> */}
+      
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <main className={styles.container}>
-        <Header userName='Tomm Holland'/>
+        <Header userName="Tomm Holland" />
         <div className={styles.content}>
           <div className={styles.instant}>
             <div className={styles.wrapper}>
@@ -45,7 +61,9 @@ function dashBoard() {
               </div>
               <div className={styles.description}>
                 <p>Quick start quiz, feel excited!!</p>
-                <Button>View <FaEye /> </Button>
+                <Button>
+                  View <FaEye />{" "}
+                </Button>
               </div>
             </div>
           </div>
